0d45fe98647a24b1adec735ad116138b9470b9a0
Cristhian Benitez <cristhiantilleria@gmail.com>
2021-09-10 07:10:17 -0400

    01-looping_a_triangle

---
+++ 01-Looping_a_triangle.js
@@ -0,0 +1,33 @@
+//EXERCISE:
+//Write a loop that makes seven calls to console.log to output the following code
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// #######
+
+//SOLUTION:
+
+let str = '';
+
+// FOR LOOP:
+
+for (let i = 0; i < 7; i++) {
+  str += '#';
+  console.log(str);
+}
+
+//WHILE LOOP:
+
+while (str.length < 7) {
+  str += '#';
+  console.log(str);
+}
+
+//DO...WHILE:
+do {
+  str += '#';
+  console.log(str);
+} while (str.length < 7);
